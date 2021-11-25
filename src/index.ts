c81d0b5ab074d617b702ad3b0db63e235aad7a30
0xtsukino <0xtsukino@gmail.com>
2021-11-25 20:51:50 +0800

    add test for frontend env

--- src/index.ts
+++ src/index.ts
@@ -31,3 +31,28 @@ export const verifyProof = (vKey: string, fullProof: any) => {
 }
 
 
+const genWnts_browser = async(input: any, wasmFilePath: string, witnessFileName: string): Promise<Uint8Array> => {
+    const resp = await fetch(wasmFilePath);
+    const buffer = await resp.arrayBuffer();
+
+    return new Promise((resolve, reject) => {
+        builder(buffer)
+        .then(async witnessCalculator => {
+            const buff= await witnessCalculator.calculateWTNSBin(input, 0);
+            resolve(buff);
+        }).catch((error) => {
+            reject(error);
+        });
+    })
+}
+
+export const genProof_browser = async (grothInput: any, wasmFilePath: string, finalZkeyPath: string) => {
+    const wntsBuff = await genWnts_browser(grothInput, wasmFilePath, 'witness.wtns');
+    const resp = await fetch(finalZkeyPath);
+    const arrayBuffer = await resp.arrayBuffer();
+
+    const { proof, publicSignals } = await groth16.prove(new Uint8Array(arrayBuffer), wntsBuff, null);
+
+    return { proof, publicSignals };
+}
+
--- test/proof.test.ts
+++ test/proof.test.ts
@@ -1,7 +1,9 @@
+import 'isomorphic-fetch';
+
 export const SNARK_FIELD_SIZE = BigInt("21888242871839275222246405745257275088548364400416034343698204186575808495617")
 const ZqField = require("ffjavascript").ZqField
 export const Fq = new ZqField(SNARK_FIELD_SIZE)
-import { genProof, verifyProof } from "../src";
+import { genProof, verifyProof, genProof_browser } from "../src";
 import { poseidon } from "circomlibjs";
 import * as fs from "fs";
 import * as path from "path";
@@ -23,6 +25,29 @@ describe('Proof test', () => {
 
         const fullProof = await genProof(grothInput, wasmFilePath, finalZkeyPath);
         const vKey = JSON.parse(fs.readFileSync(vkeyPath, "utf-8"))
+        const res = await verifyProof(vKey, fullProof);
+        expect(res).toBe(true)
+    });
+
+        it("Should create proof without fs", async () => {
+
+        const preimage: bigint = Fq.random();
+
+        const hash = poseidon([preimage]);
+
+        const wasmFilePath: string = 'http://localhost:5000/circuit.wasm';
+        const finalZkeyPath: string = 'http://localhost:5000/circuit_final.zkey';
+        const vkeyPath = 'http://localhost:5000/verification_key.json';
+        // // grothInput: any, wasmFilePath: string, finalZkeyPath: string
+        const grothInput = {
+            preimage
+        };
+
+        const fullProof = await genProof_browser(grothInput, wasmFilePath, finalZkeyPath);
+
+        const resp = await fetch(vkeyPath);
+        const text = await resp.text();
+        const vKey = JSON.parse(text)
 
         const res = await verifyProof(vKey, fullProof);
         expect(res).toBe(true)
